b6a727c8e6bbe86ce122532e6e79a388f0fc9d87
mlevos <mathias.levostre@gmail.com>
2020-05-09 16:15:49 +0200

    warning syntax

--- modules/kubernetes/variables.tf
+++ modules/kubernetes/variables.tf
@@ -1,18 +1,18 @@
-variable "google_api_key" {
-    type = "string"
+variable google_api_key {
+    type = string
     description="api key to provide resources"
 }
-variable "project_name" {
-    type = "string"
+variable project_name {
+    type = string
     description="GCP project name"
 }
 
-variable "region" {
-    type = "string"
+variable region {
+    type = string
     description="GCP region"
 }
        
-variable "k8s_cluster_name" {
-    type = "string"
+variable k8s_cluster_name {
+    type = string
     description="K8S cluster name"
 }
